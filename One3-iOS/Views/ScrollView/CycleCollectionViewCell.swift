d3318a252f448a5e8cbff49e1955f542cdc2d7e4
Binboy <binboy@live.com>
2016-12-31 15:27:15 +0800

    build read view top cycle scroll view with collection view

---
+++ One3-iOS/Views/ScrollView/CycleCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CycleCollectionViewCell.swift
+//  One3-iOS
+//
+//  Created by Binboy_王兴彬 on 2016/12/30.
+//  Copyright © 2016年 Binboy. All rights reserved.
+//
+
+import UIKit
+
+class CycleCollectionViewCell: UICollectionViewCell {
+    
+}
